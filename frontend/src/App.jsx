6d9e4045a38c70e32d14604d20d9a401d440f326
Apeksha-Kharik <kharikapeksha01@gmail.com>
2026-04-02 01:31:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import LoginPage from "./pages/LoginPage";
 import PatientDashboard from "./pages/PatientDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -15,23 +15,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/patient_dashboard"
-          element={
-            <ProtectedRoute role="Patient">
-              <PatientDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute role="Admin">
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
+        {/* Protection removed: Logic and UI remain the same */}
+        <Route path="/patient_dashboard" element={<PatientDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
